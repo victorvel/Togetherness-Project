5d1cc375c1be4f06005c7ccc061638150b0fbe8d
Victor Velasco <victorvelasco098@gmail.com>
2021-02-11 22:56:57 -0700

    changing mobile view

--- javascript/library.js
+++ javascript/library.js
@@ -16,24 +16,88 @@ let videoList = [
         video: "https://www.youtube.com/embed/VRSDXqOASdU"
     },
     {
-        name: "Christmas Time is Here - Peter Kurtz",
+        name: "christmas Time is Here - Peter Kurtz",
         video: "https://www.youtube.com/embed/xw5DORLiowk"
     },
     {
-        name: "What Child is This – The Togetherness Project",
+        name: "what Child is This – The Togetherness Project",
         video: "https://www.youtube.com/embed/vK5FO4ecmlA"
     },
     {
-        name: "Beethoven Sonata in G major, Movement 1 – The Togetherness Project",
+        name: "beethoven Sonata in G major, Movement 1 – The Togetherness Project",
         video: "https://www.youtube.com/embed/F7X-oNrshUU"
     },
     {
-        name: "Blue Christmas – Kenny Godfrey",
+        name: "blue Christmas – Kenny Godfrey",
         video: "https://www.youtube.com/embed/nF2jJcf7cP4"
     },
     {
-        name: "Away in a Manger – RJ, Nick, & Ashley",
+        name: "away in a Manger – RJ, Nick, & Ashley",
         video: "https://www.youtube.com/embed/r0GoLmAe43I"
+    },
+    {
+        name: "for now - josh",
+        video: "https://www.youtube.com/embed/7w8hiwZJuG0"
+    },
+    {
+        name: "singing Carols – W/ the Tyler Family!",
+        video: "https://www.youtube.com/embed/nsodgPnTLvw"
+    },
+    {
+        name: "sadie - Have Yourself A Merry Little Christmas",
+        video: "https://www.youtube.com/embed/Zpkd1_OEIA4"
+    },
+    {
+        name: "O Come, O Come Emmanual – Devin",
+        video: "https://www.youtube.com/embed/23s3rYCLYqQ"
+    },
+    {
+        name: "Away in a Manger – Alyssa",
+        video: "https://www.youtube.com/embed/bNEQn6Gz1Wg"
+    },
+    {
+        name: "Brad Greer",
+        video: "https://www.youtube.com/embed/iTX-_zz009Y"
+    }, 
+    {
+        name: "Alexah - I Heard The Bells On Christmas Day", 
+        video: "https://www.youtube.com/embed/KiZZsxvzNjY"
+    },
+    {
+        name: "Sabrina and Peter - God Rest Ye Merry Gentlemen", 
+        video: "https://www.youtube.com/embed/do-qXdLrdCI"
+    },
+    {
+        name: "BYU Resonance - The First Noel",
+        video: "https://www.youtube.com/embed/VqWoWFJ98LU"
+    }, 
+    {
+        name: "Mary Did You Know? - Kyler and Amber", 
+        video: "https://www.youtube.com/embed/coc2kwqBuAI"
+    }, 
+    {
+        name: "Micah on the viola", 
+        video: "https://www.youtube.com/embed/_ZYxV_F2mXE"
+    }, 
+    {
+        name: "Christmas Melodies – Nate and Emmy", 
+        video: "https://www.youtube.com/embed/EsI5lltlczk"
+    }, 
+    {
+        name: "Where are you Christmas? – Performed by Rylee", 
+        video: "https://www.youtube.com/embed/3hlonXcIA0Y"
+    }, 
+    {
+        name: "silent Night – Allison", 
+        video: "https://www.youtube.com/embed/WspEd5u_o_A"
+    }, 
+    {
+        name: "Hallelujah – Yvonne", 
+        video: "https://www.youtube.com/embed/XudxjzoRwII"
+    }, 
+    {
+        name: "Can't Help Falling in Love Cover – Boston & Brooklyn", 
+        video: "https://www.youtube.com/embed/24bZMnkrTH8"
     }
 
 ];
